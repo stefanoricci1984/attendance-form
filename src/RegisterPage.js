bae4358e9c1cda680a616fce66f47d82456996d2
stefanoricci1984 <stefanoricci222@gmail.com>
2024-08-29 22:49:52 +0200

    plo

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -12,7 +12,7 @@ const RegisterPage = () => {
         e.preventDefault();
 
         try {
-            await axios.post('https://attendance-app-backend-nine.vercel.app/api/register', {
+            await axios.post('https://attendance-app-backend-mu.vercel.app/api/register', {
                 name,
                 email,
                 password,
