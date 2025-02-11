498de0f1b7f1b9fa1fffa98ba97383b0322d0567
stefanoricci1984 <71469629+stefanoricci1984@users.noreply.github.com>
2025-02-11 19:49:44 +0100

    Update RegisterPage.js

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // Importa il componente Link
+import { Link } from 'react-router-dom'; 
 import './RegisterPage.css'; // Importa il file CSS
 
 const RegisterPage = () => {
